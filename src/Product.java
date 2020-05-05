b5753d573bdfe9337eb2a69112e930926897ba7d
rap9210 <rap9210@gmail.com>
2020-05-05 14:52:01 -0400

    Inheritance Completion Exercise I

---
+++ src/Product.java
@@ -0,0 +1,59 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Product {
+    private String code;
+    private String description;
+    private double price;
+    protected static int count = 0; // a protected static variable
+
+    public Product() {
+        code = "";
+        description = "";
+        price = 0;
+    }
+
+    // Get and set accessors for the code, description, and price instance variables
+
+    @Override
+    public String toString() {
+        return "Code:                " + code + "\n" +
+                "Description:        " + description + "\n" +
+                "Price:              " + this.getFormattedPrice(price) + "\n";
+    }
+
+    public String  getFormattedPrice(double price) {
+        // Use the NumberFormat class to format the price to 2 decimal places
+        String formatPrice = NumberFormat.getCurrencyInstance().format(price);
+
+    return formatPrice;}
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Create public access for the count variable
+    public static int getCount() {
+        return count;
+    }
+}
---
+++ src/ProductApp.java
@@ -0,0 +1,33 @@
+public class ProductApp {
+    public static void main(String[] args) {
+        Book nf001 = new Book();
+
+        nf001.setCode("001");
+        nf001.setPrice(4.99);
+        nf001.setDescription("Origin of Species");
+        nf001.setAuthor("Charles Darwin");
+        nf001.setPages(507);
+
+        System.out.println("Product Code: "+nf001.getCode()+"\nDescription: "+nf001.getDescription()+" by "+nf001.getAuthor());
+        System.out.println("Pages: "+nf001.getPages()+"\n"+nf001.getFormattedPrice(nf001.getPrice()));
+
+
+
+        Software sw002 = new Software();
+        sw002.setCode("002");
+        sw002.setPrice(99.99);
+        sw002.setDescription("Calculator");
+        sw002.setProgrammer("Microsoft");
+        sw002.setOs("Windows");
+        sw002.setPlatform("PC");
+
+        System.out.println("Product Code: "+sw002.getCode()+"\nDescription: "+sw002.getDescription()+" by "+sw002.getProgrammer());
+        System.out.println("OS: "+sw002.getOs()+"\t\tPlatform: "+sw002.getPlatform()+"\n"+sw002.getFormattedPrice(sw002.getPrice()));
+
+
+
+
+
+
+    }
+}
